948ce609a87c9c8b98a517a2fefb49cb273bb2a8
JeongGeun Park <readytojoin086@gmail.com>
2023-05-30 18:55:29 +0900

    [Gold I] Title: 구간 합 구하기, Time: 340 ms, Memory: 25332 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,61 +1,65 @@
 #include <stdio.h>
-#define SIZE 1000000
-typedef long long LL;
+#include <stdlib.h>
+#include <math.h>
 
-LL arr[SIZE];
-LL tree[SIZE * 4];
+#define ll long long
 
-void build(int start, int end, int idx) {
-	if(start == end) {
-		tree[idx] = arr[start];
-		return;
-	}
+ll init(ll* arr, ll* tree, ll node, ll start, ll end){
+    if (start == end)
+        return tree[node] = arr[start];
 
-	int mid = (start + end) / 2;
-	build(start, mid, idx * 2);
-	build(mid + 1, end, idx * 2 + 1);
-	tree[idx] = tree[idx * 2] + tree[idx * 2 + 1];
+    ll mid = (start + end) / 2;
+
+    return tree[node] = init(arr, tree, node * 2, start, mid) + init(arr, tree, node * 2 + 1, mid + 1, end);
 }
 
-void update(int start, int end, int idx, LL what, LL value) {
-	if(what < start || end < what) return;
-	if (start == end) {
-		tree[idx] = value;
-		return;
-	}
+void update(ll* tree, ll node, ll start, ll end, ll index, ll diff){
+    if (index < start || index > end) return;
 
-	int mid = (start + end) / 2;
-	update(start, mid, idx * 2, what, value);
-	update(mid + 1, end, idx * 2 + 1, what, value);
-	tree[idx] = tree[idx * 2] + tree[idx * 2 + 1];
+    tree[node] += diff;
+
+    if (start != end) {
+        ll mid = (start + end) / 2;
+        update(tree, node * 2, start, mid, index, diff);
+        update(tree, node * 2 + 1, mid + 1, end, index, diff);
+    }
 }
 
-LL interval_sum(int start, int end, int idx, LL left, LL right) {
-	if (end < left || right < start) return 0;
-	if (left <= start && end <= right) return tree[idx];
+ll sum(ll* tree, ll node, ll start, ll end, ll left, ll right){
+    if (left > end || right < start)
+        return 0;
 
-	int mid = (start + end) / 2;
-	return interval_sum(start, mid, idx * 2, left, right) + interval_sum(mid + 1, end, idx * 2 + 1, left, right);
+    if (left <= start && end <= right)
+        return tree[node];
+
+    ll mid = (start + end) / 2;
+
+    return sum(tree, node * 2, start, mid, left, right) + sum(tree, node * 2 + 1, mid + 1, end, left, right);
 }
 
-int main(void) {
-	int n, m, k;
-	scanf("%d %d %d", &n, &m, &k);
-
-	for (int i = 0; i < n; i++) 
-		scanf("%lld", &arr[i]);
-
-	build(0, n - 1, 1);
-
-	for (int i = 0; i < m + k; i++) {
-		LL a, b, c;
-		scanf("%lld %lld %lld", &a, &b, &c);
-		if (a == 1) {
-			update(0, n - 1, 1, b - 1, c);
-		}
-		else {
-			printf("%lld\n", interval_sum(0, n - 1, 1, b - 1, c - 1));
-		}
-	}
-	return 0;
+int main() {
+    ll n, m, k;
+    scanf("%lld %lld %lld", &n, &m, &k);
+    ll* arr = (ll*)malloc(sizeof(ll) * (n + 1));
+    for(ll i=1; i<=n; i++){
+        scanf("%lld", &arr[i]);
+    }
+    ll h = (ll)ceil(log2(n));
+    ll tree_size = (1 << (h + 1));
+    ll* tree = (ll*)malloc(sizeof(ll) * tree_size);
+    init(arr, tree, 1, 1, n);
+    m += k;
+    while(m--){
+        ll a, b, c;
+        scanf("%lld %lld %lld", &a, &b, &c);
+        if(a == 1){
+            ll diff = c - arr[b];
+            arr[b] = c;
+            update(tree, 1, 1, n, b, diff);
+        }
+        else{
+            printf("%lld\n", sum(tree, 1, 1, n, b, c));
+        }
+    }
+    return 0;
 }
