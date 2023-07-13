c9d2af5eccb49885ed9c277111ec4c56c2eaf7d5
JeongGeun Park <readytojoin086@gmail.com>
2023-07-13 14:26:17 +0900

    [Silver II] Title: 병사 배치하기, Time: 156 ms, Memory: 115136 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+arr = list(map(int, input().rstrip().split()))
+
+dp = [1 for _ in range(n)]
+for i in range(1, n):
+    for j in range(i):
+        if arr[j] > arr[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(n-max(dp))
