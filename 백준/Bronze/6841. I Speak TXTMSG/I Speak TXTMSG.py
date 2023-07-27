7177cd93dc598e1f784a3f8ce5e9abc45fcb48b2
JeongGeun Park <readytojoin086@gmail.com>
2023-07-27 16:39:28 +0900

    [Bronze IV] Title: I Speak TXTMSG, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/6841. I Speak TXTMSG/I Speak TXTMSG.py
@@ -0,0 +1,17 @@
+oh = {"CU":"see you",
+":-)":"I’m happy",
+":-(":"I’m unhappy",
+";-)":"wink",
+":-P":"stick out my tongue",
+"(~.~)":"sleepy",
+"TA":"totally awesome",
+"CCC":"Canadian Computing Competition",
+"CUZ":"because",
+"TY":"thank-you",
+"YW":"you’re welcome",
+"TTYL":"talk to you later"}
+
+while True:
+    string = input().rstrip()
+    print(oh[string] if oh.get(string) else string)
+    if string == 'TTYL': break
