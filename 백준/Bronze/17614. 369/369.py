96992822abe81b4acd1bcdc276c16c01ded95848
JeongGeun Park <readytojoin086@gmail.com>
2023-08-15 15:46:28 +0900

    [Bronze III] Title: 369, Time: 160 ms, Memory: 114844 KB -BaekjoonHub

---
+++ 백준/Bronze/17614. 369/369.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+cnt = 0
+for i in range(1, n+1):
+    cnt += str(i).count('3') + str(i).count('6') + str(i).count('9')
+
+print(cnt)
