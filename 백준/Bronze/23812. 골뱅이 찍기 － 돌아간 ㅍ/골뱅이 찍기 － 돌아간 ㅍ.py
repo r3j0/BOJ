bad2c2ebf7f85a5744e149e93830162dd47b2976
JeongGeun Park <readytojoin086@gmail.com>
2023-09-03 16:14:10 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅍ, Time: 132 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/23812. 골뱅이 찍기 － 돌아간 ㅍ/골뱅이 찍기 － 돌아간 ㅍ.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+
+for _ in range(2):
+    for _ in range(n):
+        print(('@'*n)+('   '*n)+('@'*n))
+    for _ in range(n):
+        print('@@@@@'*n)
+for _ in range(n):
+    print(('@'*n)+('   '*n)+('@'*n))
