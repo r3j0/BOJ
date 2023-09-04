d6cc9cef90f81972b5127b9ba3ed1180438201a4
JeongGeun Park <readytojoin086@gmail.com>
2023-09-04 14:22:49 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 128 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+for _ in range(n):
+    print('@@@@@'*n)
+for _ in range(n):
+    print('@'*n)
+for _ in range(n):
+    print('@@@@@'*n)
+for _ in range(n):
+    print('@'*n)
+for _ in range(n):
+    print('@@@@@'*n)
