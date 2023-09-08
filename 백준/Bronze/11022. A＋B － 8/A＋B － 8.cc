a12dba0eed17dc00424f5532ab59d7e62b30acb6
JeongGeun Park <readytojoin086@gmail.com>
2023-09-08 09:26:23 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    int t; scanf("%d", &t);
+    for (int i = 1; i <= t; i++) {
+        int a, b; scanf("%d %d", &a, &b);
+        printf("Case #%d: %d + %d = %d\n", i, a, b, a + b);
+    }
+    return 0;
+}
