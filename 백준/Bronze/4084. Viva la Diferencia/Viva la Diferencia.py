28755693e801f29c7d2d16a35e3263e7632779cc
JeongGeun Park <readytojoin086@gmail.com>
2023-09-10 12:17:51 +0900

    [Bronze III] Title: Viva la Diferencia, Time: 148 ms, Memory: 115196 KB -BaekjoonHub

---
+++ 백준/Bronze/4084. Viva la Diferencia/Viva la Diferencia.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    a, b, c, d = map(int, input().rstrip().split())
+    if a == b == c == d == 0: break
+
+    cnt = 0
+    while not (a == b == c == d):
+        cnt += 1
+        a, b, c, d = abs(a-b), abs(b-c), abs(c-d), abs(d-a)
+    
+    print(cnt)
