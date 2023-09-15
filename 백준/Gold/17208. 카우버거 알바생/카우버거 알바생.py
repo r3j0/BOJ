f5c38dd2975e3e85bffbf3d691f73f726662da33
JeongGeun Park <readytojoin086@gmail.com>
2023-09-15 17:14:10 +0900

    [Gold IV] Title: 카우버거 알바생, Time: 744 ms, Memory: 189708 KB -BaekjoonHub

--- 백준/Gold/17208. 카우버거 알바생/카우버거 알바생.py
+++ 백준/Gold/17208. 카우버거 알바생/카우버거 알바생.py
@@ -4,7 +4,6 @@ n, m, k = map(int, input().rstrip().split())
 arr = [list(map(int, input().rstrip().split())) for _ in range(n)]
 dp = [[[0 for _ in range(k+1)] for _ in range(m+1)] for _ in range(n+1)]
 for i in range(n+1): dp[i][0][0] = 0
-max_value = 0
 
 for burger in range(1, m+1):
     for potato in range(1, k+1):
@@ -14,8 +13,7 @@ for burger in range(1, m+1):
 
             if burger >= now_burger and potato >= now_potato:
                 dp[thing][burger][potato] = max(dp[thing - 1][burger][potato], dp[thing - 1][burger - now_burger][potato - now_potato] + 1)
-                max_value = max(max_value, dp[thing][burger][potato])
             else:
                 dp[thing][burger][potato] = dp[thing - 1][burger][potato]
 
-print(max_value)
+print(dp[n][m][k])
