72a29132686ac17a5e053bbbdcbf241f52b172b4
JeongGeun Park <readytojoin086@gmail.com>
2023-09-24 14:32:37 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -1,17 +1,31 @@
 #include <stdio.h>
 
 int main(void) {
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
-
-    if (a == b && b == c) printf("%d", 10000+a*1000);
-    else if (a == b || a == c) printf("%d", 1000+a*100);
-    else if (b == c) printf("%d", 1000+b*100);
-    else {
-        if (a > b && a > c) printf("%d", a*100);
-        else if (b > a && b > c) printf("%d", b*100);
-        else if (c > a && c > b) printf("%d", c*100);
-    }
-    
-    return 0;
+	int a, b, c;
+	int result;
+	scanf("%d %d %d", &a, &b, &c);
+	if (a == b && b == c)
+		result = 10000 + (a * 1000);
+	else if (a == b || b == c || a == c) {
+		if (a == b)
+			result = 1000 + (a * 100);
+		else
+			result = 1000 + (c * 100);
+	}
+	else {
+		if (a < b) {
+			if (b < c)
+				result = c * 100;
+			else
+				result = b * 100;
+		}
+		else {
+			if (a < c)
+				result = c * 100;
+			else
+				result = a * 100;
+		}
+	}
+	printf("%d", result);
+	return 0;
 }
