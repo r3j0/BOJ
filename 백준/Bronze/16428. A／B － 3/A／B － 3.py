275ac2825e1cc2a8db5a667c506d091af4b1dcbb
JeongGeun Park <readytojoin086@gmail.com>
2023-09-30 13:57:43 +0900

    [Bronze III] Title: A/B - 3, Time: 128 ms, Memory: 113112 KB, Score: 630 point -BaekjoonHub

--- 백준/Bronze/16428. A／B － 3/A／B － 3.py
+++ 백준/Bronze/16428. A／B － 3/A／B － 3.py
@@ -1,2 +1,14 @@
-a, b = map(int, input().split())
-print(a//b, a%b)
+import sys
+input = sys.stdin.readline
+
+a, b = map(int, input().rstrip().split())
+
+if a > 0 and b < 0:
+    print(-(a//(-b)))
+    print(a%(-b))
+elif a < 0 and b < 0 and a%b != 0:
+    print(a//b+1)
+    print(a%b-b)
+else:
+    print(a//b)
+    print(a%b)
