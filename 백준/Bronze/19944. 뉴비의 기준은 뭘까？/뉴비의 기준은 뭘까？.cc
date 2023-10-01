d8db22de4d4aee36837e5153c7d82a1d56aab594
JeongGeun Park <readytojoin086@gmail.com>
2023-10-01 17:06:49 +0900

    [Bronze IV] Title: 뉴비의 기준은 뭘까?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int n,m;
+    scanf("%d %d",&n,&m);
+    if(m<=2)
+        printf("NEWBIE!");
+    else if(m>2&&m<=n)
+        printf("OLDBIE!");
+    else
+        printf("TLE!");
+    return 0;
+}
