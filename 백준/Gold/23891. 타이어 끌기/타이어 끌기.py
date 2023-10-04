ccbbfff441fb82afb9c83310f4d8e52b7cded04b
JeongGeun Park <readytojoin086@gmail.com>
2023-10-04 12:12:45 +0900

    [Gold II] Title: 타이어 끌기, Time: 600 ms, Memory: 191584 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/23891. 타이어 끌기/타이어 끌기.py
+++ 백준/Gold/23891. 타이어 끌기/타이어 끌기.py
@@ -3,16 +3,13 @@ input = sys.stdin.readline
 
 n, m = map(int, input().rstrip().split())
 arr = [list(map(int, input().rstrip().split())) for _ in range(n)]
-size = len(arr)
 sum_value = 0
-for i in range(size):
+for i in range(n):
     sum_value += arr[i][0]
-    arr.append([arr[i][0] * 2, arr[i][1] + 1])
 
-dp = [[0 for _ in range(m+1)] for _ in range(len(arr)+1)]
-
+dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
 for weight in range(1, m+1):
-    for thing in range(1, len(arr)+1):
+    for thing in range(1, n+1):
         now_weight = arr[thing-1][1]
         now_value = arr[thing-1][0]
 
@@ -21,6 +18,15 @@ for weight in range(1, m+1):
         else:
             dp[thing][weight] = dp[thing-1][weight]
 
-if dp[len(arr)][m] > sum_value: print('W')
-elif dp[len(arr)][m] == sum_value: print('D')
+        now_weight = arr[thing-1][1] + 1
+        now_value = arr[thing-1][0] * 2
+
+        if weight >= now_weight:
+            dp[thing][weight] = max(dp[thing][weight], dp[thing-1][weight], dp[thing-1][weight-now_weight] + now_value)
+        else:
+            dp[thing][weight] = max(dp[thing][weight], dp[thing-1][weight])
+
+if dp[n][m] > sum_value: print('W')
+elif dp[n][m] == sum_value: print('D')
 else: print('L')
+       
