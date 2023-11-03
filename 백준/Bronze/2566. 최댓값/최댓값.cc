ee68009696dd4c3737b256019f4e127a656ad8a6
JeongGeun Park <readytojoin086@gmail.com>
2023-11-03 10:51:21 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.cc
+++ 백준/Bronze/2566. 최댓값/최댓값.cc
@@ -1,26 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-    int arr[9][9];
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++)
-            scanf("%d", &arr[i][j]);
-    }
+    int now;
+    int max_value = 0;
+    int max_y;
+    int max_x;
 
-    int max_value = arr[0][0];
-    int max_y = 1;
-    int max_x = 1;
+    for (int i = 0; i < 81; i++) {
+        scanf("%d", &now);
 
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            if (max_value < arr[i][j]) {
-                max_value = arr[i][j];
-                max_y = i + 1;
-                max_x = j + 1;
-            }
+        if (i == 0 || max_value < now) {
+            max_value = now;
+            max_y = i / 9;
+            max_x = i % 9;
         }
     }
 
-    printf("%d\n%d %d", max_value, max_y, max_x);
+    printf("%d\n%d %d", max_value, max_y + 1, max_x + 1);
     return 0;
 }
