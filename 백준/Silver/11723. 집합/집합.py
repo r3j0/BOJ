fc9c40e7f7e1c6bc9bb5df287afaeb80d519589c
JeongGeun Park <readytojoin086@gmail.com>
2023-11-22 15:00:30 +0900

    [Silver V] Title: 집합, Time: 1288 ms, Memory: 130256 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -2,14 +2,12 @@ import sys
 input = sys.stdin.readline
 
 m = int(input().rstrip())
-S = [0 for _ in range(21)]
+S = 0
 for _ in range(m):
     ope = list(input().rstrip().split())
-    if ope[0] == 'add': S[int(ope[1])] = 1
-    elif ope[0] == 'remove': S[int(ope[1])] = 0
-    elif ope[0] == 'check': print(S[int(ope[1])])
-    elif ope[0] == 'toggle': S[int(ope[1])] = 1 if S[int(ope[1])] == 0 else 0
-    elif ope[0] == 'all':
-        for i in range(1, 21): S[i] = 1
-    elif ope[0] == 'empty':
-        for i in range(1, 21): S[i] = 0
+    if ope[0] == 'add': S = S | (1 << (int(ope[1]) - 1))
+    elif ope[0] == 'remove': S = S & ((1 << 20) - 1 - (1 << (int(ope[1]) - 1)))
+    elif ope[0] == 'check': print((S & (1 << (int(ope[1]) - 1))) >> (int(ope[1]) - 1))
+    elif ope[0] == 'toggle': S = S ^ (1 << (int(ope[1]) - 1))
+    elif ope[0] == 'all': S = (1 << 20) - 1
+    elif ope[0] == 'empty': S = 0 
