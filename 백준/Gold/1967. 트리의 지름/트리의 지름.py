562bba4645ce95540c67f4ca3f7b832a85ff3a0e
JeongGeun Park <readytojoin086@gmail.com>
2023-12-21 14:58:45 +0900

    [Gold IV] Title: 트리의 지름, Time: 168 ms, Memory: 251520 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 5)
+
+v = int(input().rstrip())
+graph = [{} for _ in range(v+1)] 
+for _ in range(v-1):
+    a, b, w = map(int, input().rstrip().split())
+    graph[a][b] = w
+    graph[b][a] = w
+
+def find_longest(now):
+    max_value = 0
+    max_key = now
+    for key, value in graph[now].items():
+        if visited_fl[key] == 0:
+            visited_fl[key] = 1
+            res = find_longest(key)
+            now_value = value + res[1]
+            if now_value > max_value:
+                max_value = now_value
+                max_key = res[0]
+    return (max_key, max_value)    
+    
+visited_fl = [0 for _ in range(v+1)]
+visited_fl[1] = 1
+start = find_longest(1)
+
+visited_fl = [0 for _ in range(v+1)]
+visited_fl[start[0]] = 1
+print(find_longest(start[0])[1])
