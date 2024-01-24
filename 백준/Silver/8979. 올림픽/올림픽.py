28c5935eea7e79f33dd2d6a0e5acc4dcfbebbbc5
JeongGeun Park <readytojoin086@gmail.com>
2024-01-24 16:17:39 +0900

    [Silver V] Title: 올림픽, Time: 120 ms, Memory: 109240 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -8,11 +8,11 @@ grade = [0 for _ in range(n)]
 
 for i in range(n):
     if i == 0:
-        grade[arr[0][0]-1] = 1
+        grade[arr[0][0]-1] = i + 1
     else:
         if arr[i-1][1:] == arr[i][1:]:
             grade[arr[i][0]-1] = grade[arr[i-1][0]-1]
         else:
-            grade[arr[i][0]-1] = grade[arr[i-1][0]-1] + 1
+            grade[arr[i][0]-1] = i + 1
 
 print(grade[k-1])
