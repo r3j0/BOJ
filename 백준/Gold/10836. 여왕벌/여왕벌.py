95e129ae208f6b23a0527f1dc5983388d415050e
JeongGeun Park <readytojoin086@gmail.com>
2024-02-07 15:23:32 +0900

    [Gold IV] Title: 여왕벌, Time: 860 ms, Memory: 244160 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.py
+++ 백준/Gold/10836. 여왕벌/여왕벌.py
@@ -2,36 +2,32 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().rstrip().split())
-order = [list(map(int, input().rstrip().split())) for _ in range(m)]
+order = [[0] + list(map(int, input().rstrip().split())) for _ in range(m)]
 
 # 0 1 2
 # 0이 n - 1 이하라면 전체 1 증가는 확정
 # 그 이상일 떄는 1 2 해당 선까지 증가
 
-plus = [0 for _ in range(n-1)]
-edge = [1 for _ in range(2*n-1)]
+edge_plus = [0 for _ in range(2*n-1)]
 
 for day in range(m):
-    day_step = 0
-    for i in range(n-1, -1, -1):
-        while order[day][day_step] == 0:
-            day_step += 1
-        edge[n - 1 - i] += day_step
-        order[day][day_step] -= 1
+    order[day][2] += order[day][1]
+    order[day][3] += order[day][2]
+    if order[day][1] != order[day][2]:
+        edge_plus[order[day][1]] += 1
+    if order[day][2] != order[day][3]:
+        if order[day][1] == order[day][2]:
+            edge_plus[order[day][1]] += 2
+        else:
+            edge_plus[order[day][2]] += 1
 
-    for j in range(1, n):
-        while order[day][day_step] == 0:
-            day_step += 1
-        edge[n - 1 + j] += day_step
-        order[day][day_step] -= 1
-        plus[j-1] += day_step
-    
+for i in range(1, 2*n-1):
+    edge_plus[i] += edge_plus[i-1]
 
-for j in range(n-1, 2*n-1):
-    print(edge[j], end=' ')
-print()
-for i in range(1, n):
-    print(edge[n-1-i], end=' ')
-    for k in range(n-1):
-        print(1 + plus[k], end=' ')
-    print()
+for i in range(2*n-1): edge_plus[i] += 1
+
+print(' '.join(map(str, edge_plus[n-1:])))
+res = ' '.join(map(str, edge_plus[n:]))
+for i in range(n-2, -1, -1):
+    print(edge_plus[i], end=' ')
+    print(res)
