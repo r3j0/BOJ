7a7646275427eac1f22f540a7fd0bf40e6f9f77a
JeongGeun Park <readytojoin086@gmail.com>
2024-02-08 00:04:18 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -0,0 +1,5 @@
+def solution(numbers, direction):
+    answer = []
+    if direction == 'right': answer = [numbers[-1]] + numbers[:-1]
+    else: answer = numbers[1:] + [numbers[0]]
+    return answer
