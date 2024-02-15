9e888aa45462e0eb2a9201b2e2c93171a1863661
JeongGeun Park <readytojoin086@gmail.com>
2024-02-15 17:04:01 +0900

    [Silver I] Title: 신입 사원, Time: 2828 ms, Memory: 217040 KB -BaekjoonHub

--- 백준/Silver I/1946. 신입 사원/신입 사원.py
+++ 백준/Silver I/1946. 신입 사원/신입 사원.py
@@ -6,9 +6,10 @@ for _ in range(t):
     n = int(input().rstrip())
     arr = [list(map(int, input().rstrip().split())) for _ in range(n)]
     arr.sort(key=lambda x:x[0])
-    res = n
-    min1 = arr[0][1]
+
+    result = n
+    min_value = arr[0][1]
     for i in range(1, n): 
-        if arr[i][1] > min1: res -= 1
-        min1 = min(min1, arr[i][1])
-    print(res)
+        if arr[i][1] > min_value: result -= 1
+        min_value = min(min_value, arr[i][1])
+    print(result)
