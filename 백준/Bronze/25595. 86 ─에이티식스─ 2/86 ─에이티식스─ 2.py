2a4303e6c6e7325f6c3df9ceba4c5e23b10b4ccd
JeongGeun Park <readytojoin086@gmail.com>
2024-02-17 14:23:16 +0900

    [Bronze I] Title: 86 ─에이티식스─ 2, Time: 120 ms, Memory: 110592 KB -BaekjoonHub

--- 백준/Bronze/25595. 86 ─에이티식스─ 2/86 ─에이티식스─ 2.py
+++ 백준/Bronze/25595. 86 ─에이티식스─ 2/86 ─에이티식스─ 2.py
@@ -15,7 +15,6 @@ for i in range(n):
             else: even += 1
         elif arr[i][j] == 2:
             now = (i+j) % 2
-if odd != 0 and even != 0: print('Kiriya')
-elif odd == 0 and even == 0: print('Lena')
-elif (odd != 0 and now == 0) or (even != 0 and now == 1): print('Lena')
+
+if (even == 0 and now == 0) or (odd == 0 and now == 1): print('Lena')
 else: print('Kiriya')
