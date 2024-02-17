28c96041cb248e44f631152d4f11a92c157e5509
JeongGeun Park <readytojoin086@gmail.com>
2024-02-17 12:09:08 +0900

    [Silver III] Title: Four Squares, Time: 152 ms, Memory: 110924 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -7,10 +7,8 @@ input = sys.stdin.readline
 n = int(input().rstrip())
 dp = [0 for _ in range(n+1)]
 for i in range(1, n+1):
-    for j in range(1, int((i+1)**0.5)+1):
+    for j in range(1, int(i**0.5)+1):
         if i - j**2 >= 0 and (i - j**2 == 0 or dp[i - j**2] > 0):
-            if dp[i] == 0:
-                dp[i] = dp[i - j**2] + 1
-            else:
-                dp[i] = min(dp[i], dp[i - j**2] + 1)
+            if dp[i] == 0: dp[i] = dp[i - j**2] + 1
+            else: dp[i] = min(dp[i], dp[i - j**2] + 1)
 print(dp[n])
