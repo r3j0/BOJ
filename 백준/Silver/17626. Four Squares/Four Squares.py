ae3fda07f6067d9b54740ddf5421ca6e772d4918
JeongGeun Park <readytojoin086@gmail.com>
2024-02-17 12:07:39 +0900

    [Silver III] Title: Four Squares, Time: 156 ms, Memory: 110924 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -0,0 +1,16 @@
+# 50000 의 제곱근은 223.6
+# 225까지 DP
+
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+dp = [0 for _ in range(n+1)]
+for i in range(1, n+1):
+    for j in range(1, int((i+1)**0.5)+1):
+        if i - j**2 >= 0 and (i - j**2 == 0 or dp[i - j**2] > 0):
+            if dp[i] == 0:
+                dp[i] = dp[i - j**2] + 1
+            else:
+                dp[i] = min(dp[i], dp[i - j**2] + 1)
+print(dp[n])
