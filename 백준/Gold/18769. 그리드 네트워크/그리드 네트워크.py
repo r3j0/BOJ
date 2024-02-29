d3d498b1e96557e388a8250761f0e569fa54f3d2
JeongGeun Park <readytojoin086@gmail.com>
2024-03-01 00:48:50 +0900

    [Gold IV] Title: 그리드 네트워크, Time: 44 ms, Memory: 33216 KB, Score: 4 point -BaekjoonHub

--- 백준/Gold/18769. 그리드 네트워크/그리드 네트워크.py
+++ 백준/Gold/18769. 그리드 네트워크/그리드 네트워크.py
@@ -2,39 +2,34 @@ import sys
 import heapq
 input = sys.stdin.readline
 
+par = []
+def parent(now):
+    while now != par[now]:
+        now = par[now]
+    return par[now]
+
+def union(a, b):
+    a = parent(a)
+    b = parent(b)
+
+    if a < b: par[b] = a
+    else: par[a] = b
+
 t = int(input().rstrip())
 for _ in range(t):
     r, c = map(int, input().rstrip().split())
-    graph = [{} for _ in range(r*c)]
     edges = []
     for i in range(r):
         arr = list(map(int, input().rstrip().split()))
         for j in range(c-1):
-            graph[i*c+j][i*c+j+1] = arr[j]
-            graph[i*c+j+1][i*c+j] = arr[j]
-
             heapq.heappush(edges, [arr[j], i*c+j, i*c+j+1])
     for i in range(r-1):
         arr = list(map(int, input().rstrip().split()))
         for j in range(c):
-            graph[i*c+j][(i+1)*c+j] = arr[j]
-            graph[(i+1)*c+j][i*c+j] = arr[j]
-
             heapq.heappush(edges, [arr[j], i*c+j, (i+1)*c+j])
     
     par = [i for i in range(r*c)]
-    def parent(now):
-        if now != par[now]:
-            par[now] = parent(par[now])
-        return par[now]
-
-    def union(a, b):
-        a = parent(a)
-        b = parent(b)
-
-        if a < b: par[b] = a
-        else: par[a] = b
-    
+    cnt = 0
     res = 0
     while edges:
         now = heapq.heappop(edges)
