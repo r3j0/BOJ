aa8d2feb62343593cebbcb058b54b4ef435f21c2
rejo <readytojoin086@gmail.com>
2026-01-29 15:51:41 +0900

    [Gold V] Title: 흙길 보수하기, Time: 128 ms, Memory: 111620 KB -BaekjoonHub

--- 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.py
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.py
@@ -6,26 +6,14 @@ n, l = map(int, input().rstrip().split())
 arr = [list(map(int, input().rstrip().split())) for _ in range(n)]
 arr.sort(key=lambda x:x[0])
 
-# 앞
-cnt_f = 0
+cnt = 0
 start = -1
 for i in range(n):
     if arr[i][0] < start: # 시작 지점을 포함하는 웅덩이
         arr[i][0] = start
     
     now_nul = (arr[i][1] - arr[i][0]) // l + (1 if (arr[i][1] - arr[i][0]) % l != 0 else 0)
-    cnt_f += now_nul
+    cnt += now_nul
     start = arr[i][0] + now_nul * l
 
-# 뒤
-cnt_b = 0
-start = 1000000001
-for i in range(n-1, -1, -1):
-    if arr[i][1] > start: # 시작 지점을 포함하는 웅덩이
-        arr[i][1] = start
-    
-    now_nul = (arr[i][1] - arr[i][0]) // l + (1 if (arr[i][1] - arr[i][0]) % l != 0 else 0)
-    cnt_b += now_nul
-    start = arr[i][1] - now_nul * l
-
-print(min(cnt_f, cnt_b))
+print(cnt)
