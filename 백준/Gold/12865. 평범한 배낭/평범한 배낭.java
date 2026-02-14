b29fa6c00660f8f6a09f0b165abef9b27a197b6f
rejo <readytojoin086@gmail.com>
2026-02-14 19:33:49 +0900

    [Gold V] Title: 평범한 배낭, Time: 188 ms, Memory: 54100 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -28,20 +28,29 @@ public class Main {
 		br.close();
 		
 		// Knapsack DP
-		int[][] dp = new int[n+1][k+1];
+		int[][] dp = new int[n+1][k+1]; // dp[i][j] : i번째 물건까지 넣었을 때, 무게 j 까지 넣을 수 있는 상황에서의 최대 가치
 		
-		for (int weight = 1; weight <= k; weight++) {
-			for (int thing = 1; thing <= n; thing++) {
-				if (weights[thing] <= weight)
+		for (int thing = 1; thing <= n; thing++) {
+			for (int weight = 1; weight <= k; weight++) {
+				if (weights[thing] <= weight) 
+					// 지금 물건을 담을 수 있다면,
+					// 1. 이전 물건을 담을 때 무게 (weight) 까지 넣을 수 있는 상황에서의 최대 가치
+					//		-> 즉, 지금 물건을 넣지 않을 때의 가치
+					// 2. 이전 물건을 담을 때 무게 (weight - 현재 물건의 무게) 까지 넣을 수 있는 상황에서의 가치 + 지금 넣을 물건의 가치
+					//		-> 즉, 지금 물건을 넣을 때의 가치
+					// 중 얻을 수 있는 최대 가치 를 따라간다.
 					dp[thing][weight] = Math.max(dp[thing-1][weight], dp[thing-1][weight - weights[thing]] + values[thing]);
-				else
+				else 
+					// 지금 물건을 담을 수 없다면, 위의 1번 (지금 물건을 넣지 않을 때의 가치) 를 따라간다.
 					dp[thing][weight] = dp[thing-1][weight];
 			}
 		}
 		
 		// Output 시작
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		bw.write(dp[n][k] + "\n");
+		
+		// n번째 물건까지 넣었을 때, 무게 k 까지 넣을 수 있는 상황에서의 최대 가치 출력
+		bw.write(dp[n][k] + "\n"); 
 		bw.flush();
 		
 		// Output 종료
