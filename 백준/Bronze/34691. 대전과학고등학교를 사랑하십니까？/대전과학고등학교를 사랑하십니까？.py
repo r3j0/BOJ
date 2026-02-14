5b34a2d12f60ecd49e5493d725595ed83385c44c
rejo <readytojoin086@gmail.com>
2026-02-14 14:08:33 +0900

    [Bronze V] Title: 대전과학고등학교를 사랑하십니까?, Time: 128 ms, Memory: 110728 KB -BaekjoonHub

---
+++ 백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.py
@@ -0,0 +1,6 @@
+while True:
+    a = input().rstrip()
+    if a == 'animal': print('Panthera tigris')
+    elif a == 'flower': print('Forsythia koreana')
+    elif a == 'tree': print('Pinus densiflora')
+    else: break
