22d0705d72870d0457c336fb4a8ada5285fa4f01
rejo <readytojoin086@gmail.com>
2026-03-04 16:16:23 +0900

    [Gold V] Title: 벼락치기, Time: 140 ms, Memory: 115208 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.py
+++ 백준/Gold/14728. 벼락치기/벼락치기.py
@@ -1,19 +1,17 @@
+# 14728 : 벼락치기
 import sys
 input = sys.stdin.readline
 
 n, t = map(int, input().rstrip().split())
+arr = [list(map(int, input().rstrip().split())) for _ in range(n)]
 
 dp = [[0 for _ in range(t+1)] for _ in range(n+1)]
-arr = [list(map(int, input().rstrip().split())) for _ in range(n)]
 
 for weight in range(1, t+1):
     for thing in range(1, n+1):
-        now_weight = arr[thing-1][0]
-        now_value = arr[thing-1][1]
-
-        if now_weight <= weight:
-            dp[thing][weight] = max(dp[thing-1][weight], dp[thing-1][weight-now_weight] + now_value)
+        if arr[thing-1][0] <= weight:
+            dp[thing][weight] = max(dp[thing-1][weight], dp[thing-1][weight - arr[thing-1][0]] + arr[thing-1][1])
         else:
             dp[thing][weight] = dp[thing-1][weight]
 
-print(dp[thing][weight])
+print(dp[n][t])
