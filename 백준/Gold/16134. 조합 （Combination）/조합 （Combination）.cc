8553876450f9ebf6df6c4450b790537938162b87
rejo <readytojoin086@gmail.com>
2026-03-22 18:41:10 +0900

    [Gold I] Title: 조합 (Combination), Time: 32 ms, Memory: 17644 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Gold/16134. 조합 （Combination）/조합 （Combination）.cc
@@ -0,0 +1,28 @@
+// 16134 : 조합 (Combination)
+#include <iostream>
+using namespace std;
+
+// Idea : FLT, Combinatorics
+typedef long long LL;
+LL MOD = 1000000007;
+#define MAX 1000000
+LL fac[MAX+1], inv[MAX+1];
+
+LL power(LL a, LL b) {
+    if (b == 0) return 1;
+    LL tmp = power(a, b/2);
+    tmp = (tmp * tmp) % MOD;
+    if (b % 2) tmp = (tmp * a) % MOD;
+    return tmp;
+}
+
+int main() {
+    LL n, r; cin >> n >> r;
+
+    fac[0] = 1;
+    for (int i = 1; i <= MAX; i++) fac[i] = (fac[i-1] * i) % MOD;
+    inv[MAX] = power(fac[MAX], MOD - 2);
+    for (int i = MAX; i > 0; i--) inv[i-1] = (inv[i] * i) % MOD;
+
+    cout << ((fac[n] * inv[n-r]) % MOD * inv[r]) % MOD;
+}
