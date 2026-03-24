ad41422c48317b736a33241d87def06fe94c0935
rejo <readytojoin086@gmail.com>
2026-03-24 16:25:13 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 196 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -1,14 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main(void) {
-	cin.tie(NULL); ios::sync_with_stdio(false);
-	
-	int t; cin >> t;
-	
-	while(t--) {
-		int a, b; cin >> a >> b;
-		cout << a + b << "\n";
-	}
-	return 0;
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+
+    // 1. cin.tie(NULL);
+    // 2. ios::sync_with_stdio(false);
+    // 3. endl -> \n
+
+    for (int i = 0; i < t; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+
+    return 0;
 }
