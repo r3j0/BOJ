c60cc6e470925ff16dd35d5f7c7f7fc2de9434a6
rejo <readytojoin086@gmail.com>
2026-03-27 15:30:54 +0900

    [Gold IV] Title: 잘 정의된 들여쓰기, Time: 312 ms, Memory: 179400 KB -BaekjoonHub

--- 백준/Gold/35309. 잘 정의된 들여쓰기/잘 정의된 들여쓰기.py
+++ 백준/Gold/35309. 잘 정의된 들여쓰기/잘 정의된 들여쓰기.py
@@ -2,21 +2,6 @@
 import sys
 input = sys.stdin.readline
 from collections import deque
-"""
-1
- 1
- 2
-  1
- 3
-  1
-  2
-2
- 1
- 2
-3
-
-1 1 2 1 3 1 2 2 1 2 3
-"""
 
 t = int(input().rstrip())
 for _ in range(t):
@@ -25,22 +10,27 @@ for _ in range(t):
     s = deque()
     ans = 'YES'
     for a in arr:
-        if not s:
-            if a != 1:
+        if a != 1: # 들여쓰기 번호가 1이 아니라면
+            while s and s[-1] + 1 != a: # a와 연속된 번호가 나올때까지
+                                        # 그동안 등장한 많게 들여쓰기한 브랜치들 지우기
+                while s:
+                    now = s.pop()
+                    if now == 1:
+                        break
+            if not s: # 전부 다 비어버리면 NO
                 ans = 'NO'
                 break
-            s.append(a)
-        else:
-            if a != 1:
-                while s and s[-1] + 1 != a:
-                    while s:
-                        now = s.pop()
-                        if now == 1:
-                            break
-                if (not s) or (s[-1] + 1 != a):
-                    ans = 'NO'
-                    break
-                s.append(a)
-            else:
-                s.append(a)
+        # 들여쓰기 번호가 1이거나, 연속된 번호를 발견했다면
+        s.append(a)
+    
+    # 최종 점검, 들여쓰기 한 줄들이 잘 들여쓰기 되어있는지
+    while s:
+        # 들여쓰기 같은 줄들을 비운다.
+        while s:
+            now = s.pop()
+            if now == 1:
+                break
+    if s: # 줄들이 남아있으면 NO
+        ans = 'NO'
+
     print(ans)
