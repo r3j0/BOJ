ef903d9e9b62e456ceae57824e2bdcca319b72b2
rejo <readytojoin086@gmail.com>
2026-03-31 21:23:44 +0900

    [Silver V] Title: K번째 수, Time: 988 ms, Memory: 21552 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.cc
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -6,14 +6,10 @@ using namespace std;
 
 int main() {
     fastio
-    int n, k;
-    cin >> n >> k;
-
-    vector<int> a (n);
+    int n, k; cin >> n >> k;
+    vector<int> a(n);
     for (int i = 0; i < n; i++) cin >> a[i];
-
     sort(a.begin(), a.end());
     cout << a[k-1];
-
     return 0;
 }
