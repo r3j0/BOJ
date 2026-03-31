1bb17671b7d6f2fb3fc9da027114efb2248779c4
rejo <readytojoin086@gmail.com>
2026-03-31 16:21:02 +0900

    [Silver V] Title: K번째 수, Time: 1036 ms, Memory: 21552 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.cc
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define fastio ios::sync_with_stdio(false);cin.tie(NULL);
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
 
 int main() {
     fastio
-    int n, k; cin >> n >> k;
-    vector<int> a(n);
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> a (n);
     for (int i = 0; i < n; i++) cin >> a[i];
 
     sort(a.begin(), a.end());
-
     cout << a[k-1];
 
     return 0;
