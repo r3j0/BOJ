db8b18a2049ecd948033bb20c4181ed4aa475afa
rejo <readytojoin086@gmail.com>
2026-04-03 15:55:12 +0900

    [Silver IV] Title: 접미사 배열, Time: 4 ms, Memory: 2580 KB -BaekjoonHub

--- 백준/Silver/11656. 접미사 배열/접미사 배열.cc
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.cc
@@ -1,31 +1,15 @@
-#include <stdio.h>
-#include <string.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
 
-char str[1005];
-char arr[1005][1005];
-int main(void) {
-    scanf("%s", str);
-    int strSize = strlen(str);
-    for (int i = 0; i < strSize; i++) {
-        char* ptr = str+i;
-        strcpy(arr[i], ptr);
-    }
-
-    // Selection Sort
-    for (int i = 0; i < strSize - 1; i++) {
-        int min = i;
-        for (int j = i + 1; j < strSize; j++) {
-            if (strcmp(arr[min], arr[j]) > 0) min = j;
-        }
-
-        if (min != i) {
-            char tmp[1005];
-            strcpy(tmp, arr[min]);
-            strcpy(arr[min], arr[i]);
-            strcpy(arr[i], tmp);
-        }
-    }
-
-    for (int i = 0; i < strSize; i++) printf("%s\n", arr[i]);
+int main() {
+    fastio
+    string s; cin >> s;
+    vector<string> v;
+    for (int i = 0; i < s.size(); i++) v.push_back(s.substr(i));
+    sort(v.begin(), v.end());
+    for (string si : v) cout << si << '\n';
     return 0;
 }
