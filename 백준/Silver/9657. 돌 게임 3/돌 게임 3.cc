b34cb8a3297288a78cfd01ef9f0a17dd2c3e1bc1
rejo <readytojoin086@gmail.com>
2026-04-04 13:33:59 +0900

    [Silver III] Title: 돌 게임 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.cc
@@ -0,0 +1,31 @@
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef __int128_t LL;
+typedef __uint128_t ULL;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+#define F first
+#define S second
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define each(x,a) for (auto &x : a)
+#define rep(i,n) for (auto i = 0; i < (n); i++)
+#define endl '\n'
+const ll INF=INT64_MAX;
+
+int main() { fastio 
+    // 9657 : 돌 게임 3
+    int n; cin >> n;
+    cout << ((n % 7 == 0 || n % 7 == 2) ? "CY" : "SK");
+    return 0;
+}
