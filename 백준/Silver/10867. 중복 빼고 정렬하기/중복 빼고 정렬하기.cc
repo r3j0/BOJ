eb35b7923be332aade04315aa07d537af65b129a
rejo <readytojoin086@gmail.com>
2026-04-05 16:01:58 +0900

    [Silver V] Title: 중복 빼고 정렬하기, Time: 16 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.cc
@@ -0,0 +1,36 @@
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef __int128_t LL;
+typedef __uint128_t ULL;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+#define F first
+#define S second
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define each(x,a) for (auto &x : a)
+#define rep(i,s,n) for (auto i = s; i < (n); i++)
+#define endl '\n'
+const ll INF=INT64_MAX;
+
+int main() { fastio 
+    // 10867 : 중복 빼고 정렬하기
+    int n; cin >> n;
+    vi v(n); rep(i,0,n) cin >> v[i];
+    sort(all(v));
+    rep(i,0,n) {
+        if (i > 0 && v[i-1] == v[i]) continue;
+        cout << v[i] << " ";
+    }
+    return 0;
+}
