ac53dd3f6ca35a48f80079a5d92c049f5d56d175
rejo <readytojoin086@gmail.com>
2026-04-08 08:07:52 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,33 @@
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef __int128_t LL;
+typedef __uint128_t ULL;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+#define F first
+#define S second
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define each(x,a) for (auto &x : a)
+#define rep(i,s,n) for (auto i = s; i < (n); i++)
+#define endl '\n'
+const ll INF=INT64_MAX;
+
+int main() { fastio 
+    // 13909 : 창문 닫기
+    ll n;cin>>n;
+    ll now = 1;
+    while(now*now<=n) now++;
+    cout<<(now-1);
+    return 0;
+}
