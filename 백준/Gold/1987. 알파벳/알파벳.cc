9a204ba17cf2f4eac10b1bdc17d93931d2a6207c
rejo <readytojoin086@gmail.com>
2026-04-11 13:48:15 +0900

    [Gold IV] Title: 알파벳, Time: 404 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,85 +1,62 @@
-#include <stdio.h>
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
 
-char arr[30][30] = { 0, };
-int r, c;
-int max = 1;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef __int128_t LL;
+typedef __uint128_t ULL;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+#define F first
+#define S second
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define each(x,a) for (auto &x : a)
+#define rep(i,s,n) for (auto i = s; i < (n); i++)
+#define endl '\n'
+const ll INF=INT64_MAX;
 
-int stack[30] = { 0, };
-int stack_size = 0;
+int n, m;
+string board[20];
+bool vis[20][20];
+bool alpvis[26];
 
-void dfs(int sero, int garo, int count, int ssize) {
-	if (sero > 0) {
-		int done = 0;
-		int now = arr[sero - 1][garo] - 48;
-		for (int i = 0; i < stack_size; i++) {
-			if (now == stack[i]) {
-				done = 1;
-				break;
-			}
-		}
-		if (done == 0) {
-			stack[stack_size++] = now;
-			dfs(sero - 1, garo, count + 1, stack_size);
-		}
-	}
-	stack_size = ssize;
-	if (sero < r - 1) {
-		int done = 0;
-		int now = arr[sero + 1][garo] - 48;
-		for (int i = 0; i < stack_size; i++) {
-			if (now == stack[i]) {
-				done = 1;
-				break;
-			}
-		}
-		if (done == 0) {
-			stack[stack_size++] = now;
-			dfs(sero + 1, garo, count + 1, stack_size);
-		}
-	}
-	stack_size = ssize;
-	if (garo > 0) {
-		int done = 0;
-		int now = arr[sero][garo - 1] - 48;
-		for (int i = 0; i < stack_size; i++) {
-			if (now == stack[i]) {
-				done = 1;
-				break;
-			}
-		}
-		if (done == 0) {
-			stack[stack_size++] = now;
-			dfs(sero, garo - 1, count + 1, stack_size);
-		}
-	}
-	stack_size = ssize;
-	if (garo < c - 1) {
-		int done = 0;
-		int now = arr[sero][garo + 1] - 48;
-		for (int i = 0; i < stack_size; i++) {
-			if (now == stack[i]) {
-				done = 1;
-				break;
-			}
-		}
-		if (done == 0) {
-			stack[stack_size++] = now;
-			dfs(sero, garo + 1, count + 1, stack_size);
-		}
-	}
-	stack_size = ssize;
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
 
-	if (max < count)
-		max = count;
+int dfs(int x, int y) {
+    int now = 1;
+    rep(d,0,4) {
+        int nx = x + dx[d];
+        int ny = y + dy[d];
+
+        if (nx < 0 || n <= nx || ny < 0 || m <= ny || vis[nx][ny] || alpvis[board[nx][ny] - 'A']) continue;
+        vis[nx][ny] = true;
+        alpvis[board[nx][ny] - 'A'] = true;
+
+        now = max(now, dfs(nx, ny) + 1);
+
+        vis[nx][ny] = false;
+        alpvis[board[nx][ny] - 'A'] = false;
+    }
+
+    return now;
 }
 
-int main(void) {
-	scanf("%d %d", &r, &c);
-	for (int i = 0; i < r; i++) {
-		scanf("%s", arr[i]);
-	}
-	stack[stack_size++] = arr[0][0] - 48;
-	dfs(0, 0, 1, stack_size);
-	printf("%d", max);
-	return 0;
+int main() { fastio 
+    // 1987 : 알파벳
+    cin >> n >> m;
+    rep(i,0,n) cin >> board[i];
+
+    vis[0][0] = true;
+    alpvis[board[0][0] - 'A'] = true;
+    cout << dfs(0, 0);
+    return 0;
 }
